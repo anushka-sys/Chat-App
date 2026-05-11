007bb42eeb2397b6cbf7f681b50cd454deaf79b7
Anushka Wable <124691369+anushka-sys@users.noreply.github.com>
2026-05-11 23:02:04 +0530

    Fix splash screen layout

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,29 +1,83 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Splash = () => {
   return (
-    <SafeAreaView style={{flex:1}} >
-      <View style={styles.container}>
-      <Text>Splash</Text>
-      <Image 
+   <SafeAreaView style={styles.safe}>
+  <View style={styles.topSection}>
+    <Image
       style={styles.image}
-      source = {require('../assets/Illustration.png')}
-      />
+      source={require('../assets/Illustration.png')}
+    />
+  </View>
+
+  <View style={styles.bottomSection}>
+    <Text style={styles.title}>
+      Connect easily with{'\n'} your family and friends {'\n'}over countries
+    </Text>
+
+    <View style={styles.button}>
+      <TouchableOpacity style={styles.btncontainer}>
+      <Text style={styles.buttontxt}>Start Messaging</Text>
+    </TouchableOpacity>
     </View>
-    </SafeAreaView>
+  </View>
+</SafeAreaView>
   )
 }
 
 export default Splash
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-  },
-  image:{
-    height:100,
-    width:100,
-  }
+safe:{
+  flex:1,
+  backgroundColor:'#fff'
+},
+
+topSection:{
+  flex:3,
+  justifyContent:'center',
+  alignItems:'center'
+},
+
+bottomSection:{
+  flex:2,
+  justifyContent:'space-evenly',
+  alignItems:'center',
+  paddingHorizontal:24,
+  paddingBottom:30
+},
+
+image:{
+  height:220,
+  width:220,
+  resizeMode:'contain'
+},
+
+title:{
+  textAlign:'center',
+  fontSize:22,
+  fontWeight:'500',
+  color:'#0F1828'
+},
+
+btncontainer:{
+  width:'100%',
+  borderRadius:30,
+  backgroundColor:'#002DE3',
+  paddingVertical:12,
+  alignItems:'center'
+},
+
+buttontxt:{
+  fontSize:16,
+  fontWeight:'600',
+  color:'white'
+},
+button:{
+   width:'100%',
+   paddingBottom:-30,
+}
+
 })
