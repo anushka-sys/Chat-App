a9b59f5f215f02b83b9c90552c063ad4f931486c
Anushka Wable <124691369+anushka-sys@users.noreply.github.com>
2026-05-13 09:56:49 +0530

    updated files

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,100 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 
 const LoginScreen = () => {
+  const [pass, setPass] = useState('');
+  const [email, setEmail] = useState('');
+
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.safe}>
+      <View style={styles.container}>
+        <Text style={styles.header}>Login here</Text>
 
-export default LoginScreen
+        <View style={styles.input}>
+          <TextInput
+            style={styles.email}
+            placeholder="Enter mail"
+            value={email}
+            onChangeText={txt => setEmail(txt)}
+          />
+        </View>
 
-const styles = StyleSheet.create({})
+        <View style={styles.inputwarap}>
+          <TextInput
+            style={styles.email}
+            placeholder="Enter password"
+            value={pass}
+            onChangeText={txt => setPass(txt)}
+          />
+        </View>
+
+        <View style={styles.buttonwraper}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.btntext}>Sign in</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  safe: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: '500',
+    paddingTop: 50,
+  },
+  email: {
+    paddingVertical: 15,
+    paddingLeft: 15,
+  },
+  input: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 5,
+    marginTop: 50,
+  },
+  inputwarap: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 5,
+    marginTop: 30,
+  },
+  buttonwraper: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: '#002DE3',
+    borderRadius: 30,
+    marginTop: 50,
+    backgroundColor: '#002DE3',
+  },
+  button: {
+    paddingVertical: 12,
+    paddingLeft: 115,
+  },
+  btntext: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: '500',
+  },
+});
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,88 +1,131 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
+import uuid from 'react-native-uuid';
 
 const SignupScreen = () => {
+  const navigation = useNavigation();
+
+  const [name, setName] = useState('');
+  const [pass, setPass] = useState('');
+  const [email, setEmail] = useState('');
+   
+    const registerUser = () =>{
+      const userId=uuid.v4();
+      firestore().collection("users").doc(userId).set({
+        email:email,
+        password:pass,
+      }).then(res=>{
+        console.log('user created');
+      }).catch(error => {
+        console.log(error);
+      })
+    }
+
   return (
     <SafeAreaView style={styles.safe}>
       <View style={styles.container}>
-      <Text style={styles.header}>SignupScreen</Text>
-      
-      <View style={styles.input}>
-        <TextInput style={styles.email}
-         placeholder='Enter mail' />
-      </View>
+        <Text style={styles.header}>Create an account</Text>
 
-      <View style={styles.inputwarap}>
-        <TextInput style={styles.email}
-        placeholder='Enter password'
-        />
-      </View>
+        <View style={styles.input}>
+          <TextInput 
+          style={styles.email}
+           placeholder="Enter mail" 
+           value={email}
+           onChangeText={txt => setEmail(txt)}
+           />
+        </View>
 
-      <View style={styles.buttonwraper}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.btntext}>Create account</Text>
+        <View style={styles.inputwarap}>
+          <TextInput 
+          style={styles.email} 
+          placeholder="Enter password"
+          value={pass}
+          onChangeText={txt => setPass(txt)}
+          />
+        </View>
+
+        <View style={styles.buttonwraper}>
+          <TouchableOpacity 
+          onPress={()=>{registerUser()}}
+          style={styles.button}>
+            <Text style={styles.btntext}>Sign up</Text>
+          </TouchableOpacity>
+        </View>
+        
+        <TouchableOpacity 
+        style={styles.login}
+        onPress={()=>navigation.navigate('Login')}
+        >
+          <Text style={styles.logintxt}>or Log in </Text>
         </TouchableOpacity>
       </View>
-    </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SignupScreen
+export default SignupScreen;
 
 const styles = StyleSheet.create({
-  safe:{
-    flex:1,
+  safe: {
+    flex: 1,
   },
-  container:{
-    flex:1,
-    alignItems:'center',
+  container: {
+    flex: 1,
+    alignItems: 'center',
   },
-  header:{
-    fontSize:20,
-    fontWeight:'500',
-    paddingTop:50,
+  header: {
+    fontSize: 20,
+    fontWeight: '500',
+    paddingTop: 50,
   },
-  email:{
-   paddingVertical:15,
-   paddingLeft:15,
+  email: {
+    paddingVertical: 15,
+    paddingLeft: 15,
   },
-  input:{
-     width:'80%',
-     borderWidth:1,
-    borderColor:'black',
-    borderRadius:5,
-    marginTop:50,
+  input: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 5,
+    marginTop: 50,
   },
-  inputwarap:{
-    width:'80%',
-     borderWidth:1,
-    borderColor:'black',
-    borderRadius:5,
-    marginTop:30,
+  inputwarap: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 5,
+    marginTop: 30,
   },
-  buttonwraper:{
-    // width:'80%',
-    //  borderWidth:1,
-    // borderColor:'#002DE3',
-    // borderRadius:30,
-    // marginTop:50,
-    // backgroundColor:'#002DE3',
+  buttonwraper: {
+    width: '80%',
+    borderWidth: 1,
+    borderColor: '#002DE3',
+    borderRadius: 30,
+    marginTop: 50,
+    backgroundColor: '#002DE3',
   },
-  button:{
-    paddingVertical:12,
-  // paddingLeft:95,
-   width:'80%',
-     borderWidth:1,
-    borderColor:'#002DE3',
-    borderRadius:30,
-    marginTop:50,
-    backgroundColor:'#002DE3',
+  button: {
+    paddingVertical: 12,
+    paddingLeft: 115,
   },
-  btntext:{
-    color:'white',
-    fontSize:15,
-    fontWeight:'500',
-  }
-})
+  btntext: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: '500',
+  },
+  login:{
+    paddingTop:20,
+  },
+  logintxt:{
+    textDecorationLine:'underline'
+  },
+});
