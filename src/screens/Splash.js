b69702eea14558be037f218511cf5c9165a466a0
Anushka Wable <124691369+anushka-sys@users.noreply.github.com>
2026-05-15 17:40:13 +0530

    updated splash screen

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,13 +1,10 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity,useWindowDimensions } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 
-
 const Splash = () => {
   const navigation = useNavigation();
- // const {width,height} = Dimensions.get('window');
-  const {Screenwidth,Screenheight} = useWindowDimensions.get('window');
 
   return (
     <SafeAreaView style={styles.safe}>
@@ -59,8 +56,8 @@ const styles = StyleSheet.create({
   },
 
   image: {
-    height: Screenwidth*0.8,
-    width: Screenheight*0.8,
+    // height:620,
+    width: '80%',
     resizeMode: 'contain',
     paddingTop: 20,
     //aspectRatio:1,
