de1311a2adb26f8bea8eb0c09d59093ca4d78d06
Anushka Wable <124691369+anushka-sys@users.noreply.github.com>
2026-05-15 17:39:02 +0530

    updated chatscree

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React,{useEffect,useState} from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const ChatScreen = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'John Doe',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = messageArray => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messageArray),
+    );
+  };
+
   return (
-    <View>
-      <Text>ChatScreen</Text>
+    <View style={{flex:1}}>
+      <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
     </View>
-  )
-}
+  );
+};
 
-export default ChatScreen
+export default ChatScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
